c0e657bfa8a463e0a1eeed45ca4a0f1897e27221
aiXpertLab <aiXpertLab@gmail.com>
2024-05-14 18:03:16 -0400

    v1

---
+++ mysite/blog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse, Http404
+
+
+def post_list(request):
+    # posts = Post.objects.all()
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404('Post does not exist')
+
+    return render(request,
+                   'blog/post/detail.html',
+                   {'post': post})
